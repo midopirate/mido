131c96230ae51bd433745f81787ead3de465dc79
midopirate <dj.dhriwa2013@gmail.com>
2018-08-12 09:16:57 +0300

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,25 @@
+<br />
+<video id="my-video" class="video-js vjs-default-skin" controls preload="auto" width="380" height="264" poster="http://www.nationalgeographic.com/content/dam/magazine/Logos/national-geographic.jpg" data-setup="{}"> <source src="http://adtv.ercdn.net/adnatgeo/adnatgeo.m3u8" controls>
+  <source
+     src="http://f24hls-i.akamaihd.net/hls/live/221193/F24_EN_LO_HLS/master_900.m3u8"
+     type="application/x-mpegURL">
+</video>
+<script src="video.js"></script>
+<script src="videojs.hls.min.js"></script>
+<script>
+var player = videojs('example-video');
+player.play();
+</script>
+</div>
+<br />
+<br />
+<br />
+<script data-cfasync="false">
+  (function(r,e,E,m,b){E[r]=E[r]||{};E[r][b]=E[r][b]||function(){
+  (E[r].q=E[r].q||[]).push(arguments)};b=m.getElementsByTagName(e)[0];m=m.createElement(e);
+  m.async=1;m.src=("file:"==location.protocol?"https:":"")+"//s.reembed.com/G-XmOl31.js";
+  b.parentNode.insertBefore(m,b)})("reEmbed","script",window,document,"api");
+</script><br />
+
+
+
